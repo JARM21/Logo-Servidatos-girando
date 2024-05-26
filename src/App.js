256fc6514b7f00ab4cac814612bdd2fd6281e907
JARM21 <josuealejo200310@gmail.com>
2024-05-25 20:22:07 -0500

    Logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡Bienvenido a Servidatos! 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Comencemos
         </a>
       </header>
     </div>
